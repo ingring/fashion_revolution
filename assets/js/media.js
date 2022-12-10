8c9a8a83fb8408206a44872a57eaf4d7af687639
ingring <ingrid.gladheim@student.uib.no>
2022-12-10 23:30:25 +0100

    font size and video controls

--- assets/js/media.js
+++ assets/js/media.js
@@ -1,5 +1,6 @@
 //For both the video and the audio file
-//Inspiration: https://developer.mozilla.org/en-US/docs/Learn/Accessibility/Multimedia
+//Inspiration: https://developer.mozilla.org/en-US/docs/Learn/Accessibility/Multimedia 
+//and https://github.com/iandevlin/iandevlin.github.io/blob/master/mdn/video-player-styled/js/video-player.js
 
 const playPauseBtn = document.querySelector(".playpause");
 const stopBtn = document.querySelector(".stop");
@@ -7,6 +8,9 @@ const rwdBtn = document.querySelector(".rwd");
 const fwdBtn = document.querySelector(".fwd");
 const timeLabel = document.querySelector(".time");
 const player = document.querySelector("video");
+const mute = document.querySelector(".mute");
+const volinc = document.querySelector(".volinc");
+const voldec = document.querySelector(".voldec");
 
 // Included the controls attribute in the html in case the JavaScript fails for any reason, the user still has some controls available.
 // Therefore starting by removing it
@@ -16,11 +20,9 @@ playPauseBtn.onclick = () => {
     if (player.paused) {
       player.play();
       playPauseBtn.textContent = "Pause";
-      // playPauseBtn.ariaLabel = "Pause";
     } else {
       player.pause();
       playPauseBtn.textContent = "Play";
-      // playPauseBtn.ariaLabel = "Play";
     }
   };
 
@@ -28,7 +30,6 @@ playPauseBtn.onclick = () => {
     player.pause();
     player.currentTime = 0;
     playPauseBtn.textContent = "Play";
-    // playPauseBtn.ariaLabel = "Play";
   };
 
   rwdBtn.onclick = () => {
@@ -41,7 +42,6 @@ playPauseBtn.onclick = () => {
       player.pause();
       player.currentTime = 0;
       playPauseBtn.textContent = "Play";
-      // playPauseBtn.ariaLabel = "Play";
     }
   };
 
@@ -54,3 +54,55 @@ playPauseBtn.onclick = () => {
     const mediaTime = `${minuteValue}:${secondValue}`;
     timeLabel.textContent = mediaTime;
   };
+
+
+  //to mute the volume on the media, video or audio
+  function muteMedia() {
+    mute.setAttribute('data-state', player.muted ? 'unmute' : 'mute');
+
+    if (mute.dataset.state === 'mute') {
+      mute.textContent = "Mute";
+    } 
+    else {
+      mute.textContent = "Unmute";
+    }
+  }
+
+  // Check the volume
+  function checkVolume(dir=false) {
+    if (dir) {
+      let currentVolume = Math.floor(player.volume * 10) / 10;
+      if (dir === '+') {
+        if (currentVolume < 1) player.volume += 0.1;
+      }
+      else if (dir === '-') {
+        if (currentVolume > 0) player.volume -= 0.1;
+      }
+      // If the volume has been turned off, also set it as muted
+      // Note: can only do this with the custom control set as when the 'volumechange' event is raised, there is no way to know if it was via a volume or a mute change
+      if (currentVolume <= 0) {
+        player.muted = true;
+      }
+      else {
+        player.muted = false;
+      }
+    }
+    muteMedia();
+  }
+
+  player.addEventListener('volumechange', function() {
+    checkVolume();
+  }, false);
+
+  mute.addEventListener('click', function(e) {
+    player.muted = !player.muted;
+    muteMedia();
+  });
+
+  volinc.addEventListener('click', function(e) {
+    checkVolume('+');
+  });
+
+  voldec.addEventListener('click', function(e) {
+    checkVolume('-');
+  });
