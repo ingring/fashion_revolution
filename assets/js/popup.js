7c530ae035b4f1d02ce6a29a92a5a3ff0dd4f5f2
ingring <ingrid.gladheim@student.uib.no>
2022-12-11 10:41:48 +0100

    exit pop up

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -3,7 +3,7 @@
 const popUpButton = document.querySelector("#popup--btn");
 const popUpContainer = document.querySelector(".popup--container");
 const popUpFocus = document.querySelector("#popup--focus");
-const popupClose = document.querySelector("#close");
+const popupClose = document.querySelector("#exitPopup");
 
 setTimeout(function () {
 
