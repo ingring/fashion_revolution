775e5db860f2ab30bb683f5fcea5af206709d9af
ingring <ingrid.gladheim@student.uib.no>
2022-12-12 07:32:55 +0100

    focus popup

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,13 +2,12 @@
 
 const popUpButton = document.querySelector("#popup--btn");
 const popUpContainer = document.querySelector(".popup--container");
-const popUpFocus = document.querySelector("#popup--focus");
 const popupExit = document.querySelector("#exitPopup");
 
 setTimeout(function () {
 
     popUpContainer.classList.toggle("show");
-    // popUpFocus.focus(); //to focus on the element
+    popUpContainer.focus(); //to focus on the element
 
 }, 2000);
 
